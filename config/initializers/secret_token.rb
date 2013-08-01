f6bbb5ef57d0fb56d29ba273d2f065616a59852a
Roman <adamivr@yahoo.com>
2013-08-01 11:52:39 +0300

    Base Project Structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoula::Application.config.secret_token = '5cf1e07975ef9518db47433c012cd5c0ca3910ef7a59b456007595308d8e1fe601cf000f38d0c281b1b5c0bda52fe349e806ea081ee2e5fdde73004d2f47ed31'
